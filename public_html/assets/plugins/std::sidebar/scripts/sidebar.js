b4524f913eb16149eaeca2d5d0ff2c5a339a4919
muji <muji@tmpfs.org>
2021-01-06 12:36:46 +0800

    Update documentation build files.

--- public_html/assets/plugins/std::sidebar/scripts/sidebar.js
+++ public_html/assets/plugins/std::sidebar/scripts/sidebar.js
@@ -1,1 +1,12 @@
-/home/muji/.uwe/plugins/std::sidebar::1.0.1/scripts/sidebar.js
+(function sidebar() {
+  let key = "sidebar-open";
+  let el = document.getElementById("sidebar-toggle");
+  if (el && sessionStorage) {
+    el.checked = sessionStorage.getItem(key);
+    el.addEventListener("change", (e) => {
+      sessionStorage.setItem(key, e.currentTarget.checked ? "open" : "");
+    });
+    let aside = document.getElementById("sidebar");
+    if (aside) { setTimeout(() => aside.classList.add("animate"), 500) }
+  }
+})();
